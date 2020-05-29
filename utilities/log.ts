2c156e8b408e3ae9705f9e04745df2e7d211e726
Peter <p.dyumin@dunice.net>
2020-05-30 01:55:00 +0300

    Add body parser, fix Login controller

--- utilities/log.ts
+++ utilities/log.ts
@@ -1,4 +1,4 @@
-import { green } from 'https://deno.land/std@0.53.0/fmt/colors.ts';
+import { green, red } from 'https://deno.land/std@0.53.0/fmt/colors.ts';
 
 import { ENV, ENVS } from '../config/index.ts';
 
@@ -6,10 +6,12 @@ import { ENV, ENVS } from '../config/index.ts';
  * Log the text and data
  * @param {string} text - text to show
  * @param {*} data - additional value to show
+ * @param {boolean} isError - display the message in a different color
  * @returns {void}
  */
-export default function (text: string = '', data: any = null): void {
+export default function (text: string = '', data: any = null, isError: boolean = false): void {
+  const color = isError ? red : green;
   if (ENV === ENVS.dev) {
-    return console.log(green(`${text} ${data || ''}`));
+    return console.log(color(`${text} ${data || ''}`));
   }
 }
