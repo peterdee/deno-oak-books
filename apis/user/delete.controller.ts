4e4c233240adfde16b62f664bbc098f83a04ba42
Peter <p.dyumin@dunice.net>
2020-06-16 23:10:21 +0300

    Work on the User APIs

--- apis/user/delete.controller.ts
+++ apis/user/delete.controller.ts
@@ -1,14 +1,12 @@
 import { Status } from 'https://deno.land/x/oak/mod.ts';
 
-import bodyParser from '../../utilities/body-parser.ts';
 import database, { collections, generateId } from '../../database/index.ts';
-import { Context, UpdateUserData } from './types.ts';
+import { Context } from './types.ts';
 import response from '../../utilities/response.ts';
-import sanitize from '../../utilities/sanitize.ts';
 import { SERVER_MESSAGES } from '../../config/index.ts';
 
 /**
- * Update user data
+ * Delete all of the user data
  * @param {Context} ctx - context
  * @returns {Promise<void>}
  */
--- apis/user/index.ts
+++ apis/user/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
 
 import authenticate from '../../middlewares/authenticate.ts';
+import deleteUser from './delete.controller.ts';
 import get from './get.controller.ts';
 import update from './update.controller.ts';
 
 const router = new Router({ prefix: '/api' });
+router.delete('/user', authenticate, deleteUser);
 router.get('/user', authenticate, get);
 router.patch('/user', authenticate, update);
 
